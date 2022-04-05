c997b1c4afba03ef1698056d08504af0770f114f
cathyy0728 <bxiao@ualberta.ca>
2022-04-05 10:29:48 -0600

    update signin

--- app/src/main/java/com/example/qrhunter/SignupActivity.java
+++ app/src/main/java/com/example/qrhunter/SignupActivity.java
@@ -1,17 +1,24 @@
 package com.example.qrhunter;
 
+import static android.graphics.Color.BLACK;
+import static android.graphics.Color.WHITE;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.SearchView;
+import android.widget.TextView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -19,8 +26,15 @@ import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+
 //if you dont have an account, then you can signup one
 public class SignupActivity extends AppCompatActivity implements View.OnClickListener {
+
+    String account;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,20 +43,30 @@ public class SignupActivity extends AppCompatActivity implements View.OnClickLis
 
         Button btnTmp = (Button) findViewById(R.id.btnSignup);
         btnTmp.setOnClickListener(this);
+        TextView txtTmp = (TextView) findViewById(R.id.txtSignin);
+        txtTmp.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
         EditText edtTmp;
-        if (view.getId() == R.id.btnSignup) {
-            edtTmp = (EditText) findViewById(R.id.txtUserSignup);
-            String account = edtTmp.getText().toString();
-            if (account.equals("")) {
-                showMessage("Account can't be empty!");
-                edtTmp.requestFocus();
-                return;
-            }
-            saveAccount(account, "");
+        switch (view.getId()) {
+            case R.id.btnSignup:
+                edtTmp = (EditText) findViewById(R.id.txtUserSignup);
+                account = edtTmp.getText().toString();
+                if (account.equals("")) {
+                    showMessage("Account can't be empty!");
+                    edtTmp.requestFocus();
+                    return;
+                }
+                saveAccount(account, "");
+                break;
+            case R.id.txtSignin:
+                Intent intent = new Intent(SignupActivity.this, SigninActivity.class);
+                startActivity(intent);
+                break;
+            default:
+                break;
         }
     }
 
@@ -65,10 +89,15 @@ public class SignupActivity extends AppCompatActivity implements View.OnClickLis
                         edtTmp.requestFocus();
                     } else {
 //                        Log.d("TAG", "No such document");
+                        hideInput();
                         docRef.set(Account);
 
-                        Intent intent = new Intent(SignupActivity.this, MainActivity.class);
-                        startActivity(intent);
+                        TextView textView = findViewById(R.id.txtSignin);
+                        textView.setVisibility(View.VISIBLE);
+                        Bitmap bitmap = createBitmap(account);
+                        ImageView imageView = findViewById(R.id.userQRCode);
+                        imageView.setImageBitmap(bitmap);
+                        showMessage("Account sign success, please download your QRCode.");
                     }
                 } else {
                     Log.d("TAG", "get failed with ", task.getException());
@@ -78,10 +107,49 @@ public class SignupActivity extends AppCompatActivity implements View.OnClickLis
 
     }
 
+    private Bitmap createBitmap(String signInQrVl) {
+        BitMatrix result = null;
+        try {
+            result = new MultiFormatWriter().encode(signInQrVl,
+                    BarcodeFormat.QR_CODE,
+                    220,
+                    220);
+        } catch (WriterException e) {
+            e.printStackTrace();
+            return null;
+        }
+        int width = result.getWidth();
+        int height = result.getHeight();
+        int[] pixels = new int[width * height];
+        for(int i = 0; i < height; i++) {
+            int offset = i * width;
+            for (int k=0; k<width; k++) {
+                pixels[offset + k] = result.get(k, i) ? BLACK : WHITE;
+            }
+        }
+        Bitmap myBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+        myBitmap.setPixels(pixels, 0, width, 0, 0, width, height);
+        return myBitmap;
+    }
+
+    private void showInput(final EditText et) {
+        et.requestFocus();
+        InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+        imm.showSoftInput(et, InputMethodManager.SHOW_IMPLICIT);
+    }
+
+    private void hideInput() {
+        InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+        View v = getWindow().peekDecorView();
+        if (null != v) {
+            imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+        }
+    }
+
     private void showMessage(String message) {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setMessage(message);
-        builder.setTitle("Error");
+        builder.setTitle("Information");
         builder.setPositiveButton("Dismiss", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
